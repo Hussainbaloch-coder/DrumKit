b8fe1a57b72b9bf5f3ab2a5104713ca433a1bc4f
Hussainbaloch-coder <82317122+Hussainbaloch-coder@users.noreply.github.com>
2022-01-25 10:30:41 +0500

    Add files via upload

---
+++ Drum kit/index-2.js
@@ -0,0 +1,6 @@
+document.querySelector(".set").addEventListener("click",handleClicker);
+function handleClicker(){
+  // var audio = new Audio("sounds/crash.mp3");
+  // audio.play();
+   console.log(this.innerHTML);
+}
